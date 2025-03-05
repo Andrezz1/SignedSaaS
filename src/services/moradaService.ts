881d79983aa949542241d27c077a1ad099b53487
andre <Andrezz1>
2025-03-05 16:03:23 +0000

    MoradaInfo and Utilziador Info queries7

--- src/services/moradaService.ts
+++ src/services/moradaService.ts
@@ -1,8 +1,26 @@
 import { Morada } from 'wasp/entities'
-import { type GetMorada } from 'wasp/server/operations'
+import { CodigoPostal } from 'wasp/entities'
+import { type GetMorada, type GetMoradaInfo } from 'wasp/server/operations'
+import { getCodigoPostal } from './codigoPostalService'
 
 export const getMorada: GetMorada<void, Morada[]> = async (args, context) => {
   return context.entities.Morada.findMany({
     orderBy: { MoradaId: 'asc' },
   })
+}
+
+export const getMoradaInfo: GetMoradaInfo<void, Array<{ morada: Morada, codigoPostal: CodigoPostal}>> = async (args, context) => {
+  const moradas = await getMorada(args, context)
+  const codigosPostais = await getCodigoPostal(args, context)
+
+  const MoradaInfo = moradas.map(morada => {
+    const codigoPostal = codigosPostais.find(cp => cp.CodigoPostalId === morada.CodigoPostalCodigoPostalId)
+    return {
+      morada,
+      codigoPostal: codigoPostal || { CodigoPostalId: -1, Localidade: 'Unknown' },
+
+    }
+  })
+
+  return MoradaInfo
 }
