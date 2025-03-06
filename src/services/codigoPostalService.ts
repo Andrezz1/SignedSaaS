04b7b1f88326f5875eef6a7f2cdc1b8d79d176dd
andre <Andrezz1>
2025-03-06 16:43:55 +0000

    idk

--- src/services/codigoPostalService.ts
+++ src/services/codigoPostalService.ts
@@ -1,8 +1,22 @@
 import { CodigoPostal } from 'wasp/entities'
 import { type GetCodigoPostal } from 'wasp/server/operations'
+import { CreateCodigoPostal } from 'wasp/server/operations'
 
 export const getCodigoPostal: GetCodigoPostal<void, CodigoPostal[]> = async (args, context) => {
   return context.entities.CodigoPostal.findMany({
     orderBy: { CodigoPostalId: 'asc' },
   })
 }
+
+
+type CreateCodigoPostalPayLoad = Pick<CodigoPostal, 'Localidade'>
+
+
+export const createCodigoPostal: CreateCodigoPostal<CreateCodigoPostalPayLoad, CodigoPostal> = async (
+  args,
+  context
+) => {
+  return context.entities.CodigoPostal.create({
+    data: { Localidade: args.Localidade },
+  });
+};
