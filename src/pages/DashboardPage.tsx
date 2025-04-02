0170fd8c3f062c9ec910701adb12727168c20299
rodrigo <ropilar2003@gmail.com>
2025-04-02 15:17:30 +0000

    Total Socios and Socios Pagantes Cards

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,32 +1,21 @@
 import { type AuthUser } from 'wasp/auth';
-import { useQuery, getUtilizadoresInfoByTipo } from 'wasp/client/operations';
-import TotalSociosCard from '../components/dasboardCards/totalSociosCard';
+import SociosCard from '../components/dasboardCards/totalSociosCard';
+import SociosPagantesCard from '../components/dasboardCards/totalSociosPayingCard';
+import DoacoesCard from '../components/dasboardCards/totalDoacoesCard';
+import PagamentosCard from '../components/dasboardCards/totalPagamentosCard';
 import DefaultLayout from '../layout/DefaultLayout';
 
 const DashboardPage = ({ user }: { user: AuthUser }) => {
-  // Aqui definimos page e pageSize conforme necessário; no caso de apenas mostrar o total, pode ser suficiente 1 e um pageSize pequeno.
-  const { data, isLoading, error } = useQuery(getUtilizadoresInfoByTipo, {
-    page: 1,
-    pageSize: 1,
-    tipoUtilizadorId: 3, // Filtra para apenas os utilizadores cujo tipo é 3
-  });
-
   return (
     <DefaultLayout user={user}>
       <div className="p-4 flex justify-center items-center min-h-screen">
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>Error: {error.message}</div>
-        ) : (
-          // Utilizamos o total retornado da query para o card
-          <TotalSociosCard totalSocios={data?.total} />
-        )}
+        <SociosCard/>
+        <SociosPagantesCard/>
+        <PagamentosCard/>
+        <DoacoesCard/>
       </div>
     </DefaultLayout>
   );
 };
 
 export default DashboardPage;
-
-
