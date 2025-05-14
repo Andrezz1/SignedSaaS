5ed65c82b6347713d8dcdb9c3623c514713185b5
andre <Andrezz1>
2025-05-14 12:43:07 +0100

    Created getDuracaoByTipoSubscricaoId function

--- src/services/duracaoService.ts
+++ src/services/duracaoService.ts
@@ -1,5 +1,8 @@
-import { Duracao } from 'wasp/entities'
-import { type GetDuracao, } from 'wasp/server/operations'
+import { Duracao, TipoSubscricao } from 'wasp/entities'
+import { 
+  type GetDuracao,
+  type GetDuracaoByTipoSubscricaoId
+} from 'wasp/server/operations'
 import { HttpError } from 'wasp/server'
 
 export const getDuracao: GetDuracao<void, Duracao[]> = async (_args, context) => {
@@ -10,4 +13,26 @@ export const getDuracao: GetDuracao<void, Duracao[]> = async (_args, context) =>
   return context.entities.Duracao.findMany({
     orderBy: { DuracaoId: 'asc' },
   })
+}
+
+export const getDuracaoByTipoSubscricaoId: GetDuracaoByTipoSubscricaoId<Pick<TipoSubscricao, 'TipoSubscricaoID'>, Duracao[]> 
+= async (args, context) => {
+  if (!context.user) {
+    throw new HttpError(401, "Não tem permissão")
+  }
+
+  if (!args.TipoSubscricaoID) {
+    throw new Error("TipoSubscricaoID não foi fornecido")
+  }
+
+  const tipoSubscricaoDuracoes = await context.entities.TipoSubscricaoDuracao.findMany({
+    where: {
+      TipoSubscricaoID: args.TipoSubscricaoID
+    },
+    include: {
+      Duracao: true
+    }
+  })
+
+  return tipoSubscricaoDuracoes.map((tsd: { Duracao: any }) => tsd.Duracao)
 }
